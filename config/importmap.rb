7b3e3994a3d9ac879a30390d8438b8dedf7cc385
RedSkiesReaperr <theopouletsouchard@gmail.com>
2024-06-03 10:17:45 +0200

    Setup apexcharts with flowbite

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'flowbite', to: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.turbo.min.js'
 pin 'flowbite-datepicker', to: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/datepicker.turbo.min.js'
 pin "lodash.debounce" # @4.0.8
+pin "apexcharts" # @3.49.1
