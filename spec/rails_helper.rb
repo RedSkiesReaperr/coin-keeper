f031ccf9c49d92240ba5745e67aad4b83651d6cd
RedSkiesReaperr <theopouletsouchard@gmail.com>
2024-06-05 18:17:36 +0200

    Install shoulda-matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,8 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/factory_bot'
+require 'support/shoulda_matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -67,8 +69,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include FactoryBot::Syntax::Methods
 
   config.before(:all, :js, type: :system) do
     driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
