53af51654094f75a7990f71f83e91ae95e2e1bb6
RedSkiesReaperr <theopouletsouchard@gmail.com>
2024-06-13 17:15:46 +0200

    Add test for movements editing

--- spec/system/user_updates_movement_spec.rb
+++ spec/system/user_updates_movement_spec.rb
@@ -4,18 +4,94 @@ require 'rails_helper'
 
 RSpec.describe 'User updates movement', :js do
   let(:user) { create(:user) }
-  let(:movement) { create(:movement, user:) }
+  let(:movement) { create(:movement, user:, pointed: true, ignored: false) }
 
   before do
     sign_in user
+    visit edit_movement_path(movement)
   end
 
-  it 'they see the updated movement' do
-    visit edit_movement_path(movement)
+  it "can't update `supplier` field" do
+    expect(page).to have_field('movement[supplier]', disabled: true)
+  end
 
-    fill_in 'movement[comment]', with: 'My updated comment!'
-    click_on 'Update'
+  it "can't update `amount` field" do
+    expect(page).to have_field('movement[amount]', disabled: true)
+  end
 
-    expect(page).to have_content('My updated comment!')
+  context 'when editing `label` field' do
+    before do
+      fill_in 'movement[label]', with: new_label
+      click_on 'Update'
+    end
+
+    context 'with invalid value' do
+      let(:new_label) { ' ' }
+
+      it 'displays error message' do
+        expect(page).to have_content('Label can\'t be blank')
+      end
+    end
+
+    context 'with valid value' do
+      let(:new_label) { 'My new Label' }
+
+      it 'displays success message' do
+        expect(page).to have_content(I18n.t('movements.update.success'))
+      end
+
+      it 'updated the label' do
+        expect(page).to have_content(new_label)
+      end
+    end
+  end
+
+  context 'when editing `comment` field' do
+    before do
+      fill_in 'movement[comment]', with: new_comment
+      click_on 'Update'
+    end
+
+    context 'with valid value' do
+      let(:new_comment) { 'My updated comment!' }
+
+      it 'displays success message' do
+        expect(page).to have_content(I18n.t('movements.update.success'))
+      end
+
+      it 'updated the comment' do
+        expect(page).to have_content(new_comment)
+      end
+    end
+  end
+
+  context 'when editing `pointed` field' do
+    before do
+      uncheck 'movement[pointed]'
+      click_on 'Update'
+    end
+
+    it 'displays success message' do
+      expect(page).to have_content(I18n.t('movements.update.success'))
+    end
+
+    it 'updated the pointed value' do
+      expect(page).to have_field('movement[pointed]', checked: false, disabled: true)
+    end
+  end
+
+  context 'when editing `ignored` field' do
+    before do
+      check 'movement[ignored]'
+      click_on 'Update'
+    end
+
+    it 'displays success message' do
+      expect(page).to have_content(I18n.t('movements.update.success'))
+    end
+
+    it 'updated the ignored value' do
+      expect(page).to have_field('movement[ignored]', checked: true, disabled: true)
+    end
   end
 end
