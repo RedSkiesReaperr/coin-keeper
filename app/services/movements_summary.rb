dcf713c3ea0687a4d1d2a56eb441a4f72cbeb0d1
RedSkiesReaperr <theopouletsouchard@gmail.com>
2024-06-15 13:13:24 +0200

    Optimize MovementsSummary

--- app/services/movements_summary.rb
+++ app/services/movements_summary.rb
@@ -11,20 +11,16 @@ class MovementsSummary
   end
 
   def by_days
-    @period.map do |day|
-      day_movements = period_movements.where(date: day)
-
-      { day:, amount: day_movements.sum(&:amount).abs }
-    end
-  end
-
-  def days
-    @days ||= @period.map(&:day)
+    @period.map { |day| { day:, amount: period_amounts[day]&.abs || 0.0 } }
   end
 
   private
 
   def period_movements
-    @period_movements ||= @movements.within_date_range(@period)
+    @period_movements ||= @movements.within_date_range(@period).order(date: :asc)
+  end
+
+  def period_amounts
+    @period_amounts ||= period_movements.group(:date).sum(:amount)
   end
 end
