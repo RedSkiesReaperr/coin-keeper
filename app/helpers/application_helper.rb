82a71c09804f5365ca7da45e524cb1423e8d435a
RedSkiesReaperr <theopouletsouchard@gmail.com>
2024-07-05 12:51:26 +0200

    Refacto flash_classes helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,14 +4,10 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def flash_classes(type)
-    case type
-    when 'alert'
-      'text-red-800 bg-red-50 border-red-300 dark:text-red-400 dark:border-red-800'
-    when 'notice'
-      'text-green-800 bg-green-50 border-green-300 dark:text-green-400 dark:border-green-800'
-    else
-      'text-gray-800 bg-gray-50 border-gray-300 dark:text-gray-300 dark:border-gray-800'
-    end
+    flash_colors = { 'alert' => 'red', 'notice' => 'green', 'info' => 'blue', 'warn' => 'yellow', 'default' => 'gray' }
+    color = flash_colors[type] || flash_colors['default']
+
+    "text-#{color}-800 bg-#{color}-50 border-#{color}-300 dark:text-#{color}-400 dark:border-#{color}-800"
   end
 
   def render_turbo_stream_flash_messages
