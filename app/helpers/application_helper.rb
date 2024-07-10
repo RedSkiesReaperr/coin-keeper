365a4701d16d5a6a74947171194635159d700685
RedSkiesReaperr <theopouletsouchard@gmail.com>
2024-07-10 10:22:26 +0200

    Fix flash/notifications classes not applied

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,6 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def flash_classes(type)
-    flash_colors = { 'alert' => 'red', 'notice' => 'green', 'info' => 'blue', 'warn' => 'yellow', 'default' => 'gray' }
-    color = flash_colors[type] || flash_colors['default']
-
-    "text-#{color}-800 bg-#{color}-50 border-#{color}-300 dark:text-#{color}-400 dark:border-#{color}-800"
-  end
-
   def render_turbo_stream_flash_messages
     turbo_stream.prepend 'flash', partial: 'layouts/flash'
   end
--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,13 +1,13 @@
 import {Controller} from "@hotwired/stimulus"
 // Connects to data-controller="flash"
 export default class extends Controller {
-    connect() {
-        setTimeout(() => {
-            this.hide()
-        }, 5000)
-    }
+  connect() {
+    setTimeout(() => {
+      this.hide()
+    }, 5000)
+  }
 
-    hide() {
-        this.element.remove()
-    }
+  hide() {
+    this.element.remove()
+  }
 }
